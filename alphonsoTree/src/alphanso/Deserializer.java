cebbc0b58c2f9efa567fb646436b00d2be6c1b33
frozenmafia <shivajay295@gmail.com>
2023-09-09 16:40:37 +0530

    first

---
+++ alphonsoTree/src/alphanso/Deserializer.java
@@ -0,0 +1,37 @@
+package alphanso;
+
+ abstract  class Deserializer {
+	 static int t;
+	 
+	    // Decodes your encoded data to tree.
+	    public static BinaryTree deserialize(String data)
+	    {
+	        if (data == null)
+	            return null;
+	        t = 0;
+	        String[] arr = data.split(",");
+	        BinaryTree tree = new BinaryTree();
+	        tree.root = helper(arr);
+	        return tree;
+	    }
+	 
+	    public static Node helper(String[] arr)
+	    {
+	        if (arr[t].equals("#"))
+	            return null;
+	 
+	        // Create node with this item
+	        // and recur for children
+	        Node root
+	            = new Node(Integer.parseInt(arr[t]));
+	        t++;
+	        root.left = helper(arr);
+	        t++;
+	        root.right = helper(arr);
+	        return root;
+	    }
+	    
+	    
+	    
+	 
+}
